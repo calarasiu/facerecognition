9ec3c86ac85346e4587ec64007bf2991d13802da
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-10-04 12:47:45 +0100

    npm errors

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -4,10 +4,26 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className = 'particles'
+              params={particlesOptions}
+            />
       <Navigation/>
       <Logo/>
       <Rank/>
