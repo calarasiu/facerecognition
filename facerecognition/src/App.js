c2b6654cedc69af1497544439feaffeb631cf24a
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-10-04 21:44:29 +0100

    add props to input form

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -1,38 +1,54 @@
-import React from 'react';
+import React,{Component} from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import Particles from 'react-particles-js';
 
 const particlesOptions = {
   particles: {
-    line_linked: {
-      shadow: {
+    number:{
+      value: 50,
+      density: {
         enable: true,
-        color: "#3CA9D1",
-        blur: 5
+        value_area: 800
       }
     }
   }
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className = 'particles'
-              params={particlesOptions}
-            />
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
-      {/* {
-          <FaceRecognition/>} */}
-          
-    </div>
-  );
+class App extends Component {
+  constructor (){
+    super();
+    this.state = { 
+      input: ''
+    }
+  }
+
+  onButtonSubmit =()=>{
+    console.log("click");
+  }
+
+  onInputChange = (event)=>{
+    console.log(event.target.value);
+  }
+  render (){
+    return (
+      <div className="App">
+        <Particles className = 'particles'
+                params={particlesOptions}
+              />
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit}/>
+        {/* {
+            <FaceRecognition/>} */}
+            
+      </div>
+    );
+  }
 }
 
 export default App;
